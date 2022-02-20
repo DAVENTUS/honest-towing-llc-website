9458f94e23115967b1470e79923c9be8c55d389c
Elyas-Abdi <contact@daventus.com>
2022-02-19 19:29:58 -0600

    v1.1.1 beta

---
+++ code/components/util/Review.js
@@ -0,0 +1,29 @@
+// packages
+import Image from "next/image"
+
+
+export default function Review({name, initials, quality, descr}) {
+    return (
+        <div className="review-container">
+            <div className="customer flex flex-ai-c">
+                <div className="customer-initials flex flex-ai-c flex-jc-c">{initials}</div>
+                <h5>{name}</h5>
+            </div>
+            <div className="stars-container">
+                <Image 
+                    src='/stars.svg'
+                    alt="5 star rating"
+                    layout='fill'
+                    objectFit='fill'
+                    objectPosition='left'
+                />
+            </div>
+            <div className="quality-container">
+                <h3>{quality}</h3>
+            </div>
+            <div className="review-descp">
+                <p>{descr}</p>
+            </div>
+        </div>
+    )
+}
